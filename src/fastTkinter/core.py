1d1556cf8d1dcd69326812e9c55ae89147f43218
Arsalan Arish <mail@arsalanarish.com>
2026-05-02 17:54:26 +0500

    .

--- src/fastTkinter/core.py
+++ src/fastTkinter/core.py
@@ -1,7 +1,30 @@
 from tkinter import *
+from tkinter import ttk
 from pathlib import Path
 
-root = Tk()
 
-root.tk.call('lappend', 'auto_path', str(Path(__file__).parent) + '\themes\awthemes-10.4.0')
-root.tk.call('package', 'require', 'awdark')
+
+class fTk:
+    def __init__(self):
+        self._root = Tk()
+        self._root.tk.call('lappend', 'auto_path', str(Path(__file__).parent) + '\\themes\\awthemes-10.4.0')
+        
+
+    def configure(self, **cnf):
+        """ 
+        Everything to do with root window will be only done through this method 
+
+        title: str => Window title -> "Hello"
+        geometry: str => Window dimensions -> "900x600"
+        """
+        self._root.title = cnf.get("title", "")
+        self._root.geometry = cnf.get("geometry", "600x600")
+
+
+    def setTheme(self, theme: Literal['default', 'awdark', 'awarc', 'awblack', 'awlight']):
+        self._root.tk.call('package', 'require', theme)
+        ttk.Style().theme_use(theme)
+
+
+    def mainloop(self):
+        self._root.mainloop()
--- tests/test.py
+++ tests/test.py
@@ -1,1 +1,9 @@
-import fastTkinter
+from fastTkinter import fTk
+
+
+root = fTk()
+root.configure(
+    title = "Hello",
+    geometry = "800x600",
+
+)
